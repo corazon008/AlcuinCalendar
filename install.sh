f16cfeb702a1e835da58a2d2e4f511b8b32ac64f
corazon <99136079+corazon008@users.noreply.github.com>
2024-11-11 16:11:33 +0100

    Update install.sh
    Add git install

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 apt update && apt upgrade -y && \
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && \
 apt-get install -y ./google-chrome-stable_current_amd64.deb && \
-sudo apt install python3-pip -y && \
+sudo apt install python3-pip git -y && \
 pip3 install webdriver-manager && \
 mkdir AlcuinCalendarApp && cd AlcuinCalendarApp && \
 git clone https://github.com/corazon008/AlcuinCalendar.git && \
