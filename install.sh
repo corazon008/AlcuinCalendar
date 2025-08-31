987f2883eacda55417b582f28cc00e22d8502e93
corazon <99136079+corazon008@users.noreply.github.com>
2025-08-31 14:13:46 +0200

    Solve issue with install script
    not having curl
    source after installing uv
    and error with creating service

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 apt update && apt upgrade -y && \
+sudo apt install python3-pip git curl -y && \
 curl -LsSf https://astral.sh/uv/install.sh | sh && \
-sudo apt install python3-pip git -y && \
+source $HOME/.local/bin/env && \
 git clone https://github.com/corazon008/AlcuinCalendar.git && \
 cd AlcuinCalendar && \
 uv sync && \
 chmod +x run.sh && \
 
+echo "Creating systemd service..." && \
 sudo tee /etc/systemd/system/alcuin.service > /dev/null <<EOL
 [Unit]
 Description=Alcuin Calendar Service
@@ -20,8 +22,8 @@ Environment=PYTHONUNBUFFERED=1
 
 [Install]
 WantedBy=multi-user.target
-EOL && \
-
+EOL
+echo "Systemd service created." && \
 sudo systemctl daemon-reload && \
 sudo systemctl enable alcuin && \
 sudo systemctl start alcuin && \
